125d04ad91e5e66e6d06befc2b31abcb3894332a
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2024-12-05 15:45:57 +0100

    params parser ok + httpPostRequests

--- kavelag/server/src/main/kotlin/org/kavelag/project/parser/ParamsParser.kt
+++ kavelag/server/src/main/kotlin/org/kavelag/project/parser/ParamsParser.kt
@@ -1,11 +1,20 @@
 package org.kavelag.project.parser
 
-fun paramsParser(httpRequest: String): String? {
-    val paramsRegex = Regex("\\?([^ ]+)")
+import java.net.URLDecoder
 
-    val result = paramsRegex.find(httpRequest)
-    if(result != null){
-        return result.groups[1]?.value
+fun paramsParser(httpRequest: String): Map<String, String> {
+    val paramsRegex = RestRegex.paramsRegex
+    val params = mutableMapOf<String, String>()
+
+    val result = paramsRegex.findAll(httpRequest)
+
+    result.forEach {match ->
+        val key = match.groups[1]?.value
+        val value = match.groups[2]?.value
+        if (key != null && value != null) {
+            params[URLDecoder.decode(key, "UTF-8")] = URLDecoder.decode(value, "UTF-8")
+        }
     }
-    return ""
+
+    return params
 }
