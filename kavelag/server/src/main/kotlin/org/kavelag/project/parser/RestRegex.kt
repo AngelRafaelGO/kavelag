249d59c9a6b3c1c9e8eb520ef8e22a68e769c651
Angel <angelrafael.gonzalez@outlook.fr>
2024-12-05 16:22:30 +0100

    added rest extractors to parser, regex handling improvements

--- kavelag/server/src/main/kotlin/org/kavelag/project/parser/RestRegex.kt
+++ kavelag/server/src/main/kotlin/org/kavelag/project/parser/RestRegex.kt
@@ -1,7 +1,9 @@
 package org.kavelag.project.parser
 
 object RestRegex {
-    val getRegex: Regex = "^GET\\b".toRegex()
-    val postRegex: Regex = "^POST\\b".toRegex()
-    val headerRegex: Regex = "^([a-zA-Z0-9-]+): (.+)\$\n".toRegex()
+    val strictHeaderRegex: Regex = "^([a-zA-Z0-9-]+): (.+)\$\n".toRegex()
+    val flexibleHeaderRegex: Regex = "^[a-zA-Z0-9-]+:\\s.*$".toRegex()
+    val httpProtocolVersionRegex: Regex = "HTTP/([0-9]+\\.[0-9]+)".toRegex()
+    val requestedResourceRegex: Regex = "^[A-Z]+\\s([\\/\\w\\.-]*)\\sHTTP/".toRegex()
+    val methodRegex: Regex = "^[A-Z]+".toRegex()
 }
