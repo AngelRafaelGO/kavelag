e6d1c89f954c10aff3fe40c9341ae1bdb03ba0ee
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2024-12-17 16:40:17 +0100

    change name of loop listenerForParams

--- kavelag/server/src/main/kotlin/org/kavelag/project/listener/Listeners.kt
+++ kavelag/server/src/main/kotlin/org/kavelag/project/listener/Listeners.kt
@@ -5,8 +5,8 @@ import org.kavelag.project.SetUserConfigurationChannel
 
 suspend fun listenerForParams(): AppliedNetworkAction? {
 
-    for (params in SetUserConfigurationChannel.appliedNetworkAction){
-        return AppliedNetworkAction(params.appliedNetworkAction, params.params)
+    for (data in SetUserConfigurationChannel.appliedNetworkAction){
+        return AppliedNetworkAction(data.appliedNetworkAction, data.params)
     }
     return null
 }
