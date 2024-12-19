df9bdef438b4ebb534f6220952c73c0bffaa3bfa
Angel <angelrafael.gonzalez@outlook.fr>
2024-12-19 08:55:21 +0100

    fixed issues with channel empty values, fixed issues with server socket restart

--- kavelag/shared/src/commonMain/kotlin/org/kavelag/project/ChannelModels.kt
+++ kavelag/shared/src/commonMain/kotlin/org/kavelag/project/ChannelModels.kt
@@ -3,12 +3,6 @@ package org.kavelag.project
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class DestinationServerConfig(val url: String, val port: Int)
-
-@Serializable
-data class AppliedNetworkAction(val appliedNetworkAction: String, val params: Int)
-
-@Serializable
 data class HttpIncomingData(val httpIncomingData: String)
 
 @Serializable
