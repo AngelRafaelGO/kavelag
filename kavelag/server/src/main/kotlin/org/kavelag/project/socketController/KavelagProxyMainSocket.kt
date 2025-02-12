f51ce1fe031f3716eb2de56e245d152d6569d1dc
Angel <angelrafael.gonzalez@outlook.fr>
2025-02-12 13:22:56 +0100

    refactored main socket, fixed issue caused by bad body parsing in main socket

--- kavelag/server/src/main/kotlin/org/kavelag/project/socketController/KavelagProxyMainSocket.kt
+++ kavelag/server/src/main/kotlin/org/kavelag/project/socketController/KavelagProxyMainSocket.kt
@@ -2,24 +2,13 @@ package org.kavelag.project.socketController
 
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
-import io.ktor.utils.io.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import org.kavelag.project.HttpDestinationServerResponse
-import org.kavelag.project.HttpIncomingData
 import org.kavelag.project.KAVELAG_PROXY_PORT
-import org.kavelag.project.ProxyGenericInfo
-import org.kavelag.project.SetUserConfigurationChannel.destinationServerResponseDataChannel
-import org.kavelag.project.SetUserConfigurationChannel.incomingHttpDataChannel
-import org.kavelag.project.SetUserConfigurationChannel.proxyGenericInfoChannel
-import org.kavelag.project.models.NetworkException
 import org.kavelag.project.models.ProxySocketConfiguration
-import org.kavelag.project.network.networkIssueSelector
-import org.kavelag.project.parser.parseIncomingHttpRequest
-import org.kavelag.project.targetServerProcessing.callTargetServer
-import org.kavelag.project.targetServerProcessing.isPortOpen
+import org.kavelag.project.socketService.handleIncomingRequest
 import java.nio.channels.ClosedSelectorException
 
 object KavelagProxyMainSocket {
@@ -42,91 +31,8 @@ object KavelagProxyMainSocket {
                         if (socket != null) {
                             launch(Dispatchers.IO) {
                                 proxySocketConfiguration.port.forEach { port ->
-                                    if (isPortOpen(proxySocketConfiguration.url, port)) {
-                                        try {
-                                            val inputChannel = socket.openReadChannel()
-                                            val outputChannel = socket.openWriteChannel(autoFlush = true)
-                                            val requestBuilder = StringBuilder()
-                                            var line: String?
-                                            var contentLength = 0
-                                            var requestBody = ""
-
-                                            while (true) {
-                                                line = inputChannel.readUTF8Line()
-                                                if (line.isNullOrEmpty()) break
-                                                requestBuilder.appendLine(line)
-                                                // Check for "Content-Length" in case of POST requests
-                                                if (line.startsWith("Content-Length:", ignoreCase = true)) {
-                                                    contentLength = line.split(":")[1].trim().toInt()
-                                                }
-                                            }
-
-                                            if (contentLength > 0) {
-                                                requestBody = inputChannel.readPacket(contentLength).readText()
-                                            }
-
-                                            val incomingHttpRequest = requestBuilder.toString() + "\n\n" + requestBody
-
-                                            launch {
-                                                incomingHttpDataChannel.send(HttpIncomingData(incomingHttpRequest))
-                                            }
-
-                                            val parsedRequest = parseIncomingHttpRequest(incomingHttpRequest)
-                                            val isNetworkIssueApplied =
-                                                networkIssueSelector(proxySocketConfiguration.appliedNetworkAction)
-
-                                            if (isNetworkIssueApplied) {
-                                                val response = callTargetServer(
-                                                    proxySocketConfiguration.url,
-                                                    port,
-                                                    parsedRequest
-                                                )
-                                                if (response != null) {
-                                                    // TODO: apply connect stage latency
-                                                    launch {
-                                                        destinationServerResponseDataChannel.send(
-                                                            HttpDestinationServerResponse(
-                                                                "Port $port -> $response"
-                                                            )
-                                                        )
-                                                    }
-                                                    val fullResponse = buildString {
-                                                        append("HTTP/1.1 200 OK\r\n")
-                                                        append("Content-Type: text/plain\r\n")
-                                                        append("Content-Length: ${response.toByteArray().size}\r\n")
-                                                        append("Connection: close\r\n")
-                                                        append("\r\n")
-                                                        append(response)
-                                                    }
-                                                    outputChannel.writeStringUtf8(fullResponse)
-                                                    outputChannel.flush()
-                                                    outputChannel.flushAndClose()
-                                                    socket.close()
-                                                }
-                                            }
-                                        } catch (e: NetworkException) {
-                                            println("Network issue occurred: ${e.message}")
-                                            launch {
-                                                destinationServerResponseDataChannel.send(
-                                                    HttpDestinationServerResponse(
-                                                        e.message!!
-                                                    )
-                                                )
-                                            }
-                                        } catch (e: Throwable) {
-                                            println("Error handling socket: $e")
-                                        } finally {
-                                            try {
-                                                socket.close()
-                                            } catch (closeException: Throwable) {
-                                                println("Error closing socket: $closeException")
-                                            }
-                                        }
-                                    } else {
-                                        launch {
-                                            proxyGenericInfoChannel.send(ProxyGenericInfo("Port $port -> this port is closed or unavailable"))
-                                        }
-                                    }
+                                    println(port)
+                                    handleIncomingRequest(proxySocketConfiguration, port, socket)
                                 }
                             }
                         }
---
+++ kavelag/server/src/main/kotlin/org/kavelag/project/socketService/SocketService.kt
@@ -0,0 +1,119 @@
+package org.kavelag.project.socketService
+
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.kavelag.project.HttpDestinationServerResponse
+import org.kavelag.project.HttpIncomingData
+import org.kavelag.project.ProxyGenericInfo
+import org.kavelag.project.SetUserConfigurationChannel
+import org.kavelag.project.models.NetworkException
+import org.kavelag.project.models.ProxySocketConfiguration
+import org.kavelag.project.network.networkIssueSelector
+import org.kavelag.project.parser.parseIncomingHttpRequest
+import org.kavelag.project.targetServerProcessing.callTargetServer
+import org.kavelag.project.targetServerProcessing.isPortOpen
+
+suspend fun handleIncomingRequest(
+    proxySocketConfiguration: ProxySocketConfiguration,
+    port: Int,
+    socket: Socket
+) {
+    coroutineScope {
+        if (isPortOpen(proxySocketConfiguration.url, port)) {
+            try {
+                val inputChannel = socket.openReadChannel()
+                val outputChannel = socket.openWriteChannel(autoFlush = true)
+                val requestBuilder = StringBuilder()
+                var line: String?
+                var contentLength = 0
+                var requestBody = ""
+
+                while (true) {
+                    line = inputChannel.readUTF8Line()
+                    if (line.isNullOrEmpty()) break
+                    requestBuilder.appendLine(line)
+                    // Check for "Content-Length" in case of POST requests
+                    if (line.startsWith("Content-Length:", ignoreCase = true)) {
+                        contentLength = line.split(":")[1].trim().toInt()
+                    }
+                }
+
+                if (contentLength > 0) {
+                    requestBody = inputChannel.readPacket(contentLength).readText()
+                }
+
+                val incomingHttpRequest =
+                    requestBuilder.toString() + (if (requestBody.isNotEmpty()) "\r\n\r\n" else "\n\n") + requestBody
+
+                launch {
+                    SetUserConfigurationChannel.incomingHttpDataChannel.send(HttpIncomingData(incomingHttpRequest))
+                }
+
+                val parsedRequest = parseIncomingHttpRequest(incomingHttpRequest)
+                val isNetworkIssueApplied =
+                    networkIssueSelector(proxySocketConfiguration.appliedNetworkAction)
+
+                if (isNetworkIssueApplied) {
+                    val response = callTargetServer(
+                        proxySocketConfiguration.url,
+                        port,
+                        parsedRequest
+                    )
+                    if (response != null) {
+                        // TODO: apply connect stage latency
+                        launch {
+                            SetUserConfigurationChannel.destinationServerResponseDataChannel.send(
+                                HttpDestinationServerResponse(
+                                    "Port $port -> $response"
+                                )
+                            )
+                        }
+                        val fullResponse = buildString {
+                            append("HTTP/1.1 200 OK\r\n")
+                            append("Content-Type: text/plain\r\n")
+                            append("Content-Length: ${response.toByteArray().size}\r\n")
+                            append("Connection: close\r\n")
+                            append("\r\n")
+                            append(response)
+                        }
+                        outputChannel.writeStringUtf8(fullResponse)
+                        outputChannel.flush()
+                        outputChannel.flushAndClose()
+                        socket.close()
+                    } else {
+                        launch {
+                            SetUserConfigurationChannel.proxyGenericInfoChannel.send(ProxyGenericInfo("The destination server did not respond"))
+                        }
+                    }
+                } else {
+                    launch {
+                        SetUserConfigurationChannel.proxyGenericInfoChannel.send(ProxyGenericInfo("No action was applied to network -> simulating unreachable destination server"))
+                    }
+                }
+            } catch (e: NetworkException) {
+                println("Network issue occurred: ${e.message}")
+                launch {
+                    SetUserConfigurationChannel.destinationServerResponseDataChannel.send(
+                        HttpDestinationServerResponse(
+                            e.message!!
+                        )
+                    )
+                }
+            } catch (e: Throwable) {
+                println("Error handling socket: $e")
+            } finally {
+                try {
+                    socket.close()
+                } catch (closeException: Throwable) {
+                    println("Error closing socket: $closeException")
+                }
+            }
+        } else {
+            launch {
+                SetUserConfigurationChannel.proxyGenericInfoChannel.send(ProxyGenericInfo("Port $port -> this port is closed or unavailable"))
+            }
+        }
+    }
+}
