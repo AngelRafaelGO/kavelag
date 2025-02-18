bea5e1d31dd52d6f60a0e515b333eba5399219ce
Sooniii <theodulejanchen@hotmail.fr>
2025-02-18 17:04:25 +0100

    test

--- kavelag/server/src/test/kotlin/TargetServerProcessingTest.kt
+++ kavelag/server/src/test/kotlin/TargetServerProcessingTest.kt
@@ -1,4 +1,7 @@
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.kavelag.project.models.HttpRequest
+import org.kavelag.project.targetServerProcessing.callTargetServer
 
 import org.kavelag.project.targetServerProcessing.isValidUrl
 import kotlin.test.Test
@@ -8,5 +11,24 @@ class TargetServerProcessingTest {
     fun shouldReturnTrueIfValidURL() {
         // ACT & ASSERT
         assertTrue(isValidUrl("http://mysiteurl.com"))
+    }
+
+
+    @Test
+    fun shouldCallTargetServerGetMethod() = runBlocking {
+        // ARRANGE
+        val headers: MutableMap<String, String> = mutableMapOf()
+        headers["test"] = "header1"
+        headers["test2"] = "header2"
+        val url = "http://localhost";
+        val port = 8080;
+        val request = HttpRequest(
+            method = "GET",
+            requestedResource = "/test",
+            httpProtocolVersion = "1.1",
+            headers = headers)
+        // ACT
+        val actual = callTargetServer(url, port, request)
+        // ASSERT
     }
 }
