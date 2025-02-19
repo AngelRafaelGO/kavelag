684795bf7c1a594bf4e3b16b411033a599048fcb
Sooniii <theodulejanchen@hotmail.fr>
2025-02-19 15:29:59 +0100

    test postMethod

--- kavelag/server/src/test/kotlin/TargetServerProcessingTest.kt
+++ kavelag/server/src/test/kotlin/TargetServerProcessingTest.kt
@@ -7,6 +7,7 @@ import org.kavelag.project.models.HttpRequest
 import org.kavelag.project.targetServerProcessing.callTargetServer
 import org.kavelag.project.targetServerProcessing.getMethod
 import org.kavelag.project.targetServerProcessing.isValidUrl
+import org.kavelag.project.targetServerProcessing.postMethod
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -42,5 +43,49 @@ class TargetServerProcessingTest {
         coVerify(exactly = 1) { getMethod("http://test.com", 8080, mockHttpRequest) }
 
         unmockkStatic(::getMethod)
+    }
+
+    @Test
+    fun shouldCallTargetServerWithPOSTRequest() = runTest {
+        //ARRANGE
+        val expectedTargetServerResponse =
+            """HTTP/1.1 200 OK
+            Content-Type: application/json
+            
+            {
+            "status": "success",
+            "message": "Requête traitée avec succès",
+            "data": {
+                "id": 123,
+                "name": "Exemple",
+                "created_at": "2025-02-19T12:00:00Z"
+            }
+        }"""
+        val mockHttpRequest = HttpRequest(
+            method = "POST",
+            requestedResource = "/api/data",
+            httpProtocolVersion = "HTTP/1.1",
+            headers = mutableMapOf("Content-Type" to "application/json"),
+            body = """{
+                "status": "success",
+                "message": "Requête traitée avec succès",
+                "data": {
+                    "id": 123,
+                    "name": "Exemple",
+                    "created_at": "2025-02-19T12:00:00Z"
+                }
+            }"""
+        )
+
+        mockkStatic(::postMethod)
+        coEvery { postMethod("http://test.com", 8080, mockHttpRequest) } returns expectedTargetServerResponse
+
+        // Act
+        val result = callTargetServer("http://test.com", 8080, mockHttpRequest)
+        // Assert
+        assertEquals(expectedTargetServerResponse, result)
+        coVerify(exactly = 1) { postMethod("http://test.com", 8080, mockHttpRequest) }
+
+        unmockkStatic(::postMethod)
     }
 }
